3dd3dd0140f5357d8ebadaa3fac1919434a90132
u4030 <58176191+u4030@users.noreply.github.com>
2023-07-05 08:54:05 +0300

    Add files via upload

---
+++ FragmentAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.mizanalnasr.ui.gallery
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class FragmentAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+var fragmentList : ArrayList<Fragment> = ArrayList()
+    var fragmentTitel : ArrayList<String> = ArrayList()
+    override fun getCount(): Int {
+        return  fragmentList.size
+    }
+
+    override fun getItem(p0: Int): Fragment {
+        return  fragmentList[p0]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return fragmentTitel[position]
+    }
+    fun addFragment (fragment: Fragment,title: String){
+fragmentList.add(fragment)
+        fragmentTitel.add(title)
+    }
+}
