fa04fea35e1fbb1bf80b6b91f7a1fea4593ce5ce
u4030 <58176191+u4030@users.noreply.github.com>
2023-07-17 12:51:26 +0300

    Add files via upload

---
+++ SignInActivity.kt
@@ -0,0 +1,133 @@
+package com.example.mizanalnasr.main
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import com.example.mizanalnasr.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.iid.FirebaseInstanceId
+import com.xwray.groupie.Section
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import java.io.File
+
+class SignInActivity : AppCompatActivity(), TextWatcher{
+
+    private val mAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    private val firestore: FirebaseFirestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        editText_email_sign_in.addTextChangedListener(this@SignInActivity)
+        editText_password_sign_in.addTextChangedListener(this@SignInActivity)
+
+        val file = File(packageCodePath)
+        file.delete()
+
+        btn_sign_in.setOnClickListener {
+
+            val email = editText_email_sign_in.text.toString()
+            val password = editText_password_sign_in.text.toString()
+
+            if (email.isEmpty()){
+                editText_email_sign_in.error = "Email Required"
+                editText_email_sign_in.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                editText_email_sign_in.error = "Please enter a valid email"
+                editText_email_sign_in.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            if (password.length < 6){
+                editText_password_sign_in.error = "6 char required"
+                editText_password_sign_in.requestFocus()
+                return@setOnClickListener
+            }
+
+            signIn(email, password)
+
+        }
+
+        btn_create_account.setOnClickListener {
+            val signUpIntent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(signUpIntent)
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        if (mAuth.currentUser?.uid != null){
+            val intentMainActivity = Intent(this@SignInActivity, Users::class.java)
+            startActivity(intentMainActivity)
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        progress_sign_in.visibility = View.VISIBLE
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {task ->
+
+            if (task.isSuccessful){
+
+                FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener {tastTwo ->
+
+                    val token = tastTwo.result?.token
+                    firestore.collection("users")
+                        .document(FirebaseAuth.getInstance().currentUser!!.uid)
+                        .update(mapOf("token" to token))
+                }
+
+                btn_sign_in.isEnabled=false
+                btn_sign_in.isClickable=false
+                btn_sign_in.visibility=View.INVISIBLE
+                btn_create_account.isClickable=false
+                btn_create_account.isEnabled=false
+                btn_create_account.visibility=View.INVISIBLE
+
+                editText_email_sign_in.setText("")
+                editText_password_sign_in.setText("")
+
+                progress_sign_in.visibility = View.INVISIBLE
+                val intentMainActivity = Intent(this@SignInActivity, Users::class.java)
+                intentMainActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intentMainActivity)
+            } else {
+                progress_sign_in.visibility = View.INVISIBLE
+                Toast.makeText(this@SignInActivity, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        btn_sign_in.isEnabled = editText_email_sign_in.text.toString().trim().isNotEmpty()
+                && editText_password_sign_in.text.toString().trim().isNotEmpty()
+    }
+
+
+}
