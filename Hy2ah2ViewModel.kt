f20b0e754231b9dce74c3f415ebe379677f11e82
u4030 <58176191+u4030@users.noreply.github.com>
2023-09-20 00:56:18 +0300

    Add files via upload

---
+++ Hy2ah2ViewModel.kt
@@ -0,0 +1,78 @@
+package com.example.markizalhadidi.ui.hay2ah
+
+import androidx.lifecycle.ViewModel
+
+class Hy2ah2ViewModel : ViewModel() {
+
+    var chhy2ahtow1 = false
+    var prichy2ahtow1 =""
+    var chhy2ahtow2 = false
+    var prichy2ahtow2 =""
+    var chhy2ahtow3 = false
+    var prichy2ahtow3 =""
+    var chhy2ahtow4 = false
+    var prichy2ahtow4 =""
+    var chhy2ahtow5 = false
+    var prichy2ahtow5 =""
+    var chhy2ahtow6 = false
+    var prichy2ahtow6 =""
+    var chhy2ahtow7 = false
+    var prichy2ahtow7 =""
+    var chhy2ahtow8 = false
+    var prichy2ahtow8 =""
+    var chhy2ahtow9 = false
+    var prichy2ahtow9 =""
+    var chhy2ahtow10 = false
+    var prichy2ahtow10 =""
+    var chhy2ahtow11 = false
+    var prichy2ahtow11 =""
+    var chhy2ahtow12 = false
+    var prichy2ahtow12 =""
+    var chhy2ahtow13 = false
+    var prichy2ahtow13 =""
+    var chhy2ahtow14 = false
+    var prichy2ahtow14 =""
+    var chhy2ahtow15 = false
+    var prichy2ahtow15 =""
+    var chhy2ahtow16 = false
+    var prichy2ahtow16 =""
+    var chhy2ahtow17 = false
+    var prichy2ahtow17 =""
+    var chhy2ahtow18 = false
+    var prichy2ahtow18 =""
+    var chhy2ahtow19 = false
+    var prichy2ahtow19 =""
+    var chhy2ahtow20 = false
+    var prichy2ahtow20 =""
+    var chhy2ahtow21 = false
+    var prichy2ahtow21 =""
+    var chhy2ahtow22 = false
+    var prichy2ahtow22 =""
+
+    val prichy2ahtow = Array(22){ ""}
+    val chtexts : Array<String> = Array(22) { "" }
+
+    var chtext1 : String = "بكس كفة خلفية عرضية"
+    var chtext2 : String = "بكس كفة معاير خلفي"
+    var chtext3 : String = "بكس كفة سفلية الأمامي"
+    var chtext4 : String = "بكس شحمة خلفي"
+    var chtext5 : String = "بكس كفة عرضية المسطرة"
+    var chtext6 : String = "بكس كفة سفلية أمامية الخلفي"
+    var chtext7 : String = "بكس هب سفلي خلفي"
+    var chtext8 : String = "صنوبرص"
+    var chtext9 : String = "بيلة عجل"
+    var chtext10 : String = "قاعدة صنوبرص علوية"
+    var chtext11 : String = "تحشير لقم بريك"
+    var chtext12 : String = "بيلية قاعدة صنوبرص"
+    var chtext13 : String = "طقم اصلاح مجموعة ستيرنج"
+    var chtext14 : String = "بكسات الدنجل"
+    var chtext15 : String = "قاعدة محرك"
+    var chtext16 : String = "قشرة دنجل"
+    var chtext17 : String = "كفة"
+    var chtext18 : String = "كفة H خلفية"
+    var chtext19 : String = "كفة خلفية عرضية"
+    var chtext20 : String = "كفة قرن علوية خلفية"
+    var chtext21 : String = "مصلب ستيرنج"
+    var chtext22 : String = "ملف طارة ستيرنج"
+
+}
